76a00c327acac921fbdd60a6ccae66342b13c430
Int Class <54957432+intclassproject@users.noreply.github.com>
2019-10-30 19:49:02 +0200

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {default = "eu-west-3"}
+variable "count" {}
+variable "cred_file" { default = "/var/jenkins_home/aws_keys/credentials" }
+variable "instanceType" {}
+variable "aws_vpc" {}
+variable "instanceName" {}
+variable "serviceName" {}
+variable "envName" {}
+variable "serviceVersion" {}
+variable "releaseVersion" {}
+variable "publicip" {}
+variable "subnet_scope" {}
+variable "securitygroup" {}
+variable "subnetName" {}
+variable "purpose" {}
