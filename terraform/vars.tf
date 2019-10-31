9f843a25b95ed6c20dcb53187afb4b43f7e27002
yossibnv <yossibnv@gmail.com>
2019-10-31 22:28:35 +0200

    add new files to release1.0.0

---
+++ terraform/vars.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {default = "us-east-1"}
+variable "count" {}
+variable "cred_file" { default = "/var/jenkins_home/aws_keys/credentials" }
+variable "instanceType" {}
+variable "aws_vpc" {}
+variable "instanceName" {}
+variable "serviceName" {}
+variable "envName" {}
+variable "serviceVersion" {}
+variable "releaseVersion" {}
+variable "publicip" {}
+variable "subnet_scope" {}
+variable "securitygroup" {}
+variable "subnetName" {}
